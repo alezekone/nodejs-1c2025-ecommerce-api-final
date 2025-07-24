b19f981f5410b5fe1594be1b7f4f0765a29f7b67
Alejandro E. Aguilar <alezek@gmail.com>
2025-07-23 22:12:09 -0300

    Ahora devuelve status 200 junto con los productos.

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -17,7 +17,7 @@ import * as Model from '../models/products.model.js'
 export const getAllProducts = async (req, res) => {
     try {
         const products = await Model.getAllProducts();
-        res.json(products);
+        res.status(200).json(products);
     } catch (error) {
         res.status(500).json({
             message: 'Error al obtener los productos',
