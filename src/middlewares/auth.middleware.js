84d0dd82d481eff019fae2bc42bbbeeaabe9d2da
Alejandro E. Aguilar <alezek@gmail.com>
2025-07-23 21:02:36 -0300

    Cambios por pruebas 3.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,8 +6,9 @@ export const auth = (req, res, next) => {
     console.log(req.headers);
 
     if (!token) {
-        return res.status(401).json({ message: 'Token no proporcionado' });
-    }
+        return res.status(401).json({ message: 'Token no proporcionado',
+            headers: req.headers });
+    };
 
     jwt.verify(token, process.env.JWT_SECRET, (err) => {
         if (err) {
